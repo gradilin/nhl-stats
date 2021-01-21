3220dd658dbd1a5cc91a2b38614852dc743ca2a3
grady <gvanpinx@gmail.com>
2021-01-20 19:43:51 -0500

    returning a promise requires async and await

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Team } from './team.model';
+import { TeamsService } from './teams.service';
 
 @Controller('teams')
-export class TeamsController {}
+export class TeamsController {
+  constructor(private teamsService: TeamsService) {}
+
+  // @Get()
+  // findAll(): string {
+  //     return 'This is a string there is nothing to find'
+  // }
+
+  @Get()
+  async returnTasks(): Promise<Team[]> {
+    return await this.teamsService.findAll();
+  }
+}
